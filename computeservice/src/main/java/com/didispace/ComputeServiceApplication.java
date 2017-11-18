d3fc48f89fc6509394d2ee6cb4139b17830cb3bc
unknown <ex-zhangsheng@dashuf.local>
2017-11-18 11:19:31 +0800

    新练习的项目上线

---
+++ computeservice/src/main/java/com/didispace/ComputeServiceApplication.java
@@ -0,0 +1,15 @@
+package com.didispace;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient//注解，该注解能激活Eureka中的DiscoveryClient实现，才能实现Controller中对服务信息的输出。
+@SpringBootApplication
+public class ComputeServiceApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(ComputeServiceApplication.class).web(true).run(args);
+	}
+
+}
