a5e800cf4207ae9e51f9a06f7bdfdf2a2411e19d
ex-zhangsheng <ex-zhangsheng@DS-TS-0239.dashuf.local>
2017-11-20 11:14:59 +0800

    添加  Feign 到项目里面，遇到 一个问题就是  Feign  与 swagger2 架包冲突的问题，要将  架包升级到  2.5.0 以上

--- computeservice/src/main/java/com/didispace/ComputeServiceApplication.java
+++ computeservice/src/main/java/com/didispace/ComputeServiceApplication.java
@@ -3,13 +3,24 @@ package com.didispace;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
-@EnableDiscoveryClient//注解，该注解能激活Eureka中的DiscoveryClient实现，才能实现Controller中对服务信息的输出。
+
+
+/*注解，该注解能激活Eureka中的DiscoveryClient实现，
+才能实现Controller中对服务信息的输出。(允许服务被发现，被调用)
+Fengn 实现的声明式服务调用客户端
+*
+*/
+
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients //注解开启Feign功能   在应用主类中通过@EnableFeignClients注解开启Feign功能
 public class ComputeServiceApplication {
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(ComputeServiceApplication.class).web(true).run(args);
+		//SpringApplication.run(ComputeServiceApplication.class, args);
 	}
 
 }
--- computeservice/src/main/java/com/didispace/web/ComputeController.java
+++ computeservice/src/main/java/com/didispace/web/ComputeController.java
@@ -8,23 +8,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import springfox.documentation.annotations.ApiIgnore;
-
 @RestController
 public class ComputeController {
 
     private final Logger logger = Logger.getLogger(getClass());
 
     @Autowired
-    private DiscoveryClient client;
-    @ApiIgnore
+    private DiscoveryClient client;  //服务的 注册的 ip 服务名 地址 等信息  的查看（Eureka 的实例对象）
+   
     @RequestMapping(value = "/add" ,method = RequestMethod.GET)
     public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
         ServiceInstance instance = client.getLocalServiceInstance();
         Integer r = a + b;
-        logger.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
+       logger.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
         return r;
     }
-
+   
+   
 }
