451a5bfe36bbe43f704db30b0cd99fa3da2f685f
ex-zhangsheng <ex-zhangsheng@DS-TS-0239.dashuf.local>
2017-12-20 16:46:52 +0800

    测试

--- springBootzs/src/main/java/com/zhangsheng/SpringBootzsServiceApplication.java
+++ springBootzs/src/main/java/com/zhangsheng/SpringBootzsServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@SpringBootApplication
+@SpringBootApplication//zhuju 
 public class SpringBootzsServiceApplication {
 	
 	 public static void main(String[] args) {
