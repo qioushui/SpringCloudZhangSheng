f69df28d069028fe2e0dbd9b3c9f14925466727f
Administrator <Administrator@MS-20180326KREW>
2018-10-09 11:12:29 +0800

    整合mybatis 与 springboot结合的代码提交

--- springBootzs/src/main/java/com/zhangsheng/dao/GirlRep.java
+++
@@ -1,9 +0,0 @@
-package com.zhangsheng.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.zhangsheng.entity.Girl;
-
-public interface GirlRep extends JpaRepository<Girl,Integer>{
-	
-}
